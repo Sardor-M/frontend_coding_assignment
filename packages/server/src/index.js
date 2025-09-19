73a5d425f74df88a3c1b8e00cd2f40c93935d76c
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-09-19 15:12:52 +0900

    fix: minor fix to cors issue added

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -10,10 +10,13 @@ app.use(
         origin: process.env.FRONTEND_URL || 'http://localhost:5173',
         credentials: true,
         methods: ['GET', 'POST', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Accept'],
+        allowedHeaders: ['Content-Type', 'Accept', 'Cache-Control'],
+        exposedHeaders: ['X-RateLimit-Remaining'],
+        optionsSuccessStatus: 204,
     })
 );
 app.use(express.json());
+app.options('/answer', cors(corsOptions));
 
 const projectRoutes = require('./routes/projects');
 const answerRoutes = require('./routes/answer');
